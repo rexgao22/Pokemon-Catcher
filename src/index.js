5eeab61964df95f87851005b78ca2ae3c9005136
Rex Gao <rexgao2013@gmail.com>
2021-01-06 16:37:23 -0800

    add music button function

--- src/index.js
+++ src/index.js
@@ -3,18 +3,41 @@ const LevelOne = require("./Pokemon/level");
 document.addEventListener("DOMContentLoaded", () => {
   const canvasEl = document.getElementById("mycanvas");
   const playbtn = document.getElementById("play-btn");
+  const introbtn = document.getElementById("game-intro");
+  const backbtn = document.getElementById("back");
   const game = new Game(canvasEl);
+  const musicbtn = document.getElementById("music-btn");
+  const audio = document.getElementById("audio");
 
   game.setCanvas();
   game.bindKeyListener();
-  game.gameCountDown();
+  //   game.gameCountDown();
   game.start();
-  document.getElementById("play-btn").onclick = function () {
+  playbtn.onclick = function () {
     document.getElementById("menus").style.display = "none";
     canvasEl.style.display = "block";
-    // game.setCanvas();
-    // game.bindKeyListener();
-    // game.gameCountDown();
-    // game.start();
+    game.gameCountDown();
   };
+
+  introbtn.onclick = function () {
+    document.getElementById("menus").style.display = "none";
+    document.getElementById("intro").style.display = "block";
+  };
+  
+  backbtn.onclick = function () {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("menus").style.display = "block";
+  };
+
+  musicbtn.onclick= function() {
+    if (audio.paused) {
+      audio.play();
+      document.getElementById("music-btn").style.backgroundImage =
+      "url(../../dist/image/musicStop.png)";
+    } else if (audio.played) {
+        audio.pause();
+        document.getElementById("music-btn").style.backgroundImage =
+        "url(../../dist/image/musicPlay.png)";
+    }
+  }
 });
